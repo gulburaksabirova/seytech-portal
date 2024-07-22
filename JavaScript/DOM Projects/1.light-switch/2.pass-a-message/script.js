2edc19dc84edf767a03e9e8665b86706bf328d6d
Gulburak <gulburak.sabirova1@gmail.com>
2024-07-22 14:26:13 +0300

    finished JS DOM/Pass a message

--- JavaScript/DOM Projects/1.light-switch/2.pass-a-message/script.js
+++ JavaScript/DOM Projects/1.light-switch/2.pass-a-message/script.js
@@ -1,7 +1,16 @@
 
 const onSubmit = () =>{
-    const val = document.querySelector("#input").value;
-   document.querySelector("#input").innerHTML = val.toUpperCase();
+    let val = document.querySelector("#text").value;
+    if(val.length > 0){
+        document.querySelector("#result").innerHTML = val.toUpperCase();
+        document.querySelector("#text").value = " ";
+    }
+        document.querySelector("#text").addEventListener("keypress", function(event){
+            if(event.key === "Enter"){
+                event.preventDefault();
+                onSubmit();
+            }
+        })
 }
-document.querySelector("#result").addEventListener("click", onSubmit);
+
 
