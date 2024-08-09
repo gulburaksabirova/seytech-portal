46ec290712d2eeae574ca31f8de00c496cc06d60
Gulburak <gulburak.sabirova1@gmail.com>
2024-08-10 00:40:18 +0300

    added crud

---
+++ JavaScript/ClassNotes/01-04-24 HOF(CRUD)/app.js
@@ -0,0 +1,18 @@
+const selectSortBy = document.querySelector("select");
+const addStudentBtn = document.getElementById("add-student");
+// --------------- Spinner ---------------
+const bsSpinner = document.getElementById("spinner");
+// --------------- Search ---------------
+const searchInput = document.getElementById("search");
+const searchGlassIcon = document.getElementById("search-glass");
+const clearIcon = document.getElementById("clear");
+// --------------- Modal ---------------
+const myModalEl = document.getElementById("myModal");
+const modalForm = document.getElementById("modalForm");
+const delConfirmTxt = document.getElementById("delete-confirmation-text");
+const liveToast = document.getElementById("liveToast");
+const tableBody = document.querySelector("tbody");
+const submitBtn = document.querySelector("#submit");
+const modalTitle = document.querySelector(".modal-header h1");
+const modalBody = document.querySelector("#myModal .modal-body");
+const modalFooter = document.querySelector("#myModal .modal-footer");
